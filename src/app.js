07d1ee321764375c0da14884c54e95649309ed17
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-02-16 16:49:21 +0100

    Homework done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log('Hallo daar!');
+const box = document.getElementById('countrybox');
+
+async function addCountries() {
+  const json = await fetch('https://restcountries.com/v3.1/all').then(async (res) => await res.json());
+  const countryList = [];
+
+  for (let i = 0; i < json.length; i++) {
+    countryList.push(json[i]);
+  }
+
+  countryList.sort((a, b) => a.population - b.population);
+
+  const list = document.createElement('ul');
+
+  countryList.forEach((country) => {
+    const item = document.createElement('li');
+
+    item.innerHTML = `<p class="countryHeader ${country.region.toLowerCase()}">${country.flag} ${country.name.common}</p><br /><p class="countryPopulation">Has a population of ${country.population} people.</p><br/>`;
+
+    list.appendChild(item);
+  });
+
+  box.appendChild(list);
+
+  console.log(countryList);
+}
+
+addCountries();
